de8e98c9f8cbbdcae6ae1777cde777d37c363439
wanqianshijie <135097639+wanqianshijie@users.noreply.github.com>
2023-06-30 00:09:40 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,121 +1,81 @@
-// GitHub API URL for retrieving photos
-const apiUrl = 'https://api.github.com/repos/wanqianshijie/photos/contents/img';
-
-// Number of photos to load initially and on scroll
-const photosPerPage = 4;
-
-// Load photos from GitHub API
-async function loadPhotos() {
-    try {
-        const response = await fetch(apiUrl);
-        const photos = await response.json();
-
-        // Randomly select photosPerPage number of photos
-        const randomPhotos = getRandomElements(photos, photosPerPage);
-
-        // Display photos on the page
-        randomPhotos.forEach(photo => displayPhoto(photo));
-    } catch (error) {
-        console.error('Error loading photos:', error);
+window.addEventListener('DOMContentLoaded', () => {
+  const galleryElement = document.getElementById('gallery');
+  let photoCount = 0;
+  
+  // Function to load and display photos
+  const loadPhotos = () => {
+    // Make a request to GitHub API to fetch photos from a repository
+    fetch('https://api.github.com/repos/wanqianshijie/photos/contents/img')
+      .then(response => response.json())
+      .then(data => {
+        const photos = data.filter(item => item.type === 'file');
+        const photoBatch = photos.slice(photoCount, photoCount + 4);
+        
+        photoBatch.forEach(photo => {
+          const photoContainer = document.createElement('div');
+          photoContainer.classList.add('photo-container');
+          
+          const img = document.createElement('img');
+          img.classList.add('photo');
+          img.src = photo.download_url;
+          img.onload = () => {
+            // Use exif.js to extract photo information
+            EXIF.getData(img, () => {
+              const photoInfo = document.createElement('div');
+              photoInfo.classList.add('photo-info');
+              photoInfo.innerHTML = `
+                <p>Date: ${formatDate(EXIF.getTag(img, 'DateTimeOriginal'))}</p>
+                <p>Latitude: ${formatCoordinate(EXIF.getTag(img, 'GPSLatitude'))}</p>
+                <p>Longitude: ${formatCoordinate(EXIF.getTag(img, 'GPSLongitude'))}</p>
+                <p>Exposure Time: ${EXIF.getTag(img, 'ExposureTime')}</p>
+                <p>Aperture: ${EXIF.getTag(img, 'FNumber')}</p>
+                <p>ISO: ${EXIF.getTag(img, 'ISOSpeedRatings')}</p>
+                <p>Device: ${EXIF.getTag(img, 'Model')}</p>
+              `;
+              photoContainer.appendChild(photoInfo);
+            });
+          };
+          
+          photoContainer.appendChild(img);
+          galleryElement.appendChild(photoContainer);
+        });
+        
+        photoCount += 4;
+      })
+      .catch(error => {
+        console.log('Error fetching photos:', error);
+      });
+  };
+  
+  // Format the coordinate value (convert from degrees to decimal format)
+  const formatCoordinate = (coordinate) => {
+    if (!coordinate) return '';
+    
+    const degrees = coordinate[0].numerator / coordinate[0].denominator;
+    const minutes = coordinate[1].numerator / coordinate[1].denominator;
+    const seconds = coordinate[2].numerator / coordinate[2].denominator;
+    
+    const decimalDegrees = degrees + minutes / 60 + seconds / 3600;
+    
+    return decimalDegrees.toFixed(6);
+  };
+  
+  // Format the date value (convert to a desired format)
+  const formatDate = (date) => {
+    if (!date) return '';
+    
+    const [year, month, day] = date.split(':');
+    
+    return `${day}/${month}/${year}`;
+  };
+  
+  // Load initial batch of photos
+  loadPhotos();
+  
+  // Load more photos when scrolling to the bottom of the page
+  window.addEventListener('scroll', () => {
+    if (window.innerHeight + window.pageYOffset >= document.body.offsetHeight) {
+      loadPhotos();
     }
-}
-
-// Display a single photo on the page
-function displayPhoto(photo) {
-    const gallery = document.getElementById('gallery');
-
-    // Create photo container
-    const photoContainer = document.createElement('div');
-    photoContainer.classList.add('photo-container');
-
-    // Create image element
-    const image = document.createElement('img');
-    image.src = photo.download_url;
-    image.alt = photo.name;
-    image.classList.add('photo');
-
-    // Create photo info element
-    const info = document.createElement('div');
-    info.classList.add('info');
-
-    // Fetch EXIF data using exif.js
-    EXIF.getData(image, function () {
-        const exifData = EXIF.getAllTags(this);
-
-        // Extract relevant information
-        const {
-            DateTimeOriginal,
-            GPSLatitude,
-            GPSLongitude,
-            ExposureTime,
-            FNumber,
-            ISOSpeedRatings,
-            Model
-        } = exifData;
-
-        // Format date
-        const formattedDate = formatDateTime(DateTimeOriginal);
-
-        // Create info HTML
-        const infoHTML = `
-            <p>Date: ${formattedDate}</p>
-            <p>Latitude: ${formatCoordinates(GPSLatitude)}</p>
-            <p>Longitude: ${formatCoordinates(GPSLongitude)}</p>
-            <p>Exposure Time: ${ExposureTime}</p>
-            <p>Aperture: ${FNumber}</p>
-            <p>ISO: ${ISOSpeedRatings}</p>
-            <p>Device: ${Model}</p>
-        `;
-
-        // Insert info HTML into info element
-        info.innerHTML = infoHTML;
-    });
-
-    // Append image and info to photo container
-    photoContainer.appendChild(image);
-    photoContainer.appendChild(info);
-
-    // Append photo container to the gallery
-    gallery.appendChild(photoContainer);
-}
-
-// Helper function to format date and time
-function formatDateTime(dateTime) {
-    const date = new Date(dateTime);
-    const formattedDate = date.toLocaleString('en-US', {
-        year: 'numeric',
-        month: 'long',
-        day: 'numeric',
-        hour: 'numeric',
-        minute: 'numeric',
-        second: 'numeric'
-    });
-    return formattedDate;
-}
-
-// Helper function to format coordinates
-function formatCoordinates(coordinates) {
-    const degrees = coordinates[0].numerator / coordinates[0].denominator;
-    const minutes = coordinates[1].numerator / coordinates[1].denominator;
-    const seconds = coordinates[2].numerator / coordinates[2].denominator;
-    const direction = coordinates[3];
-
-    const formattedCoordinates = `${degrees}° ${minutes}' ${seconds}" ${direction}`;
-    return formattedCoordinates;
-}
-
-// Helper function to get a random subset of elements from an array
-function getRandomElements(array, numElements) {
-    const shuffled = array.sort(() => 0.5 - Math.random());
-    return shuffled.slice(0, numElements);
-}
-
-// Load initial set of photos
-loadPhotos();
-
-// Load more photos on scroll
-window.addEventListener('scroll', function () {
-    if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
-        loadPhotos();
-    }
+  });
 });
