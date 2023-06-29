60ba4bf43948b3d801dbade8df92e389d7d0dd46
wanqianshijie <135097639+wanqianshijie@users.noreply.github.com>
2023-06-30 01:26:44 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,108 +1,109 @@
-// 使用GitHub API获取照片列表
-const githubUsername = 'wanqianshijie';
-const repoName = 'photos';
-const path = 'img';
-
-fetch(`https://api.github.com/repos/${githubUsername}/${repoName}/contents/${path}`)
+window.onload = function() {
+  // 使用GitHub API获取照片列表
+  fetch('https://api.github.com/repos/wanqianshijie/photos/contents/img')
     .then(response => response.json())
     .then(data => {
-        // 遍历照片列表
-        data.forEach(photo => {
-            const imageUrl = photo.download_url;
-            
-            // 创建照片元素
-            const photoElement = document.createElement('img');
-            photoElement.src = imageUrl;
-            photoElement.classList.add('photo');
-            
-            // 在照片下方创建exif信息元素
-            const exifInfoElement = document.createElement('div');
-            exifInfoElement.classList.add('exif-info');
-            
-            // 使用exif.js获取exif信息
-            EXIF.getData(photoElement, function() {
-                const exifData = EXIF.getAllTags(this);
-                
-                // 解析并显示exif信息
-                const exifInfo = parseExifData(exifData);
-                exifInfoElement.innerText = exifInfo;
-            });
-            
-            // 将照片和exif信息添加到页面上
-            const photoContainer = document.getElementById('photo-container');
-            photoContainer.appendChild(photoElement);
-            photoContainer.appendChild(exifInfoElement);
-        });
+      // 遍历照片列表并展示
+      data.forEach(photo => {
+        if (photo.name.endsWith('.jpg') || photo.name.endsWith('.jpeg') || photo.name.endsWith('.png')) {
+          displayPhoto(photo.download_url);
+        }
+      });
     })
-    .catch(error => {
-        console.error('Error:', error);
-    });
+    .catch(error => console.error(error));
+};
 
-// 解析exif信息
-function parseExifData(exifData) {
-    let exifInfo = '';
-    
-    if (exifData.DateTimeOriginal) {
-        const date = new Date(exifData.DateTimeOriginal);
-        const formattedDate = formatDate(date);
-        exifInfo += `拍摄时间：${formattedDate}\n`;
-    }
-    
-    if (exifData.GPSLatitude && exifData.GPSLongitude) {
-        const latitude = parseCoordinate(exifData.GPSLatitude, exifData.GPSLatitudeRef);
-        const longitude = parseCoordinate(exifData.GPSLongitude, exifData.GPSLongitudeRef);
-        exifInfo += `经度坐标：${longitude}\n`;
-        exifInfo += `纬度坐标：${latitude}\n`;
-    }
-    
-    if (exifData.ExposureTime) {
-        const exposureTime = parseFraction(exifData.ExposureTime);
-        exifInfo += `曝光时间：${exposureTime}\n`;
-    }
-    
-    if (exifData.FNumber) {
-        exifInfo += `光圈大小：f/${exifData.FNumber}\n`;
-    }
-    
-    if (exifData.ISO) {
-        exifInfo += `ISO：${exifData.ISO}\n`;
-    }
-    
-    if (exifData.Make && exifData.Model) {
-        exifInfo += `设备信息：${exifData.Make} ${exifData.Model}\n`;
-    }
-    
-    return exifInfo;
+function displayPhoto(photoUrl) {
+  // 创建照片元素
+  var photoElement = document.createElement('img');
+  photoElement.src = photoUrl;
+
+  // 监听照片加载完成事件
+  photoElement.onload = function() {
+    // 获取照片的EXIF信息
+    var exifData = EXIF.getData(photoElement);
+    var exifInfo = getFormattedExifInfo(exifData);
+
+    // 创建照片容器
+    var photoContainer = document.createElement('div');
+    photoContainer.classList.add('photo-container');
+
+    // 创建照片描述
+    var descriptionElement = document.createElement('div');
+    descriptionElement.classList.add('photo-description');
+    descriptionElement.innerText = exifInfo;
+
+    // 将照片和描述添加到页面中
+    photoContainer.appendChild(photoElement);
+    photoContainer.appendChild(descriptionElement);
+    document.getElementById('photo-container').appendChild(photoContainer);
+  };
 }
 
-// 格式化日期
-function formatDate(date) {
-    const year = date.getFullYear();
-    const month = String(date.getMonth() + 1).padStart(2, '0');
-    const day = String(date.getDate()).padStart(2, '0');
-    return `${year}-${month}-${day}`;
+function getFormattedExifInfo(exifData) {
+  // 提取EXIF信息
+  var dateTimeOriginal = exifData.DateTimeOriginal;
+  var latitude = exifData.GPSLatitude;
+  var longitude = exifData.GPSLongitude;
+  var exposureTime = exifData.ExposureTime;
+  var apertureValue = exifData.ApertureValue;
+  var isoSpeedRatings = exifData.ISOSpeedRatings;
+  var make = exifData.Make;
+  var model = exifData.Model;
+
+  // 优化经度和纬度坐标
+  var latitudeRef = exifData.GPSLatitudeRef;
+  var longitudeRef = exifData.GPSLongitudeRef;
+  latitude = optimizeCoordinate(latitude, latitudeRef);
+  longitude = optimizeCoordinate(longitude, longitudeRef);
+
+  // 优化拍摄日期格式
+  dateTimeOriginal = formatDateTimeOriginal(dateTimeOriginal);
+
+  // 优化曝光时间格式
+  exposureTime = formatExposureTime(exposureTime);
+
+  // 构建EXIF信息字符串
+  var exifInfo = "拍摄时间：" + dateTimeOriginal + "\n";
+  exifInfo += "经度坐标：" + longitude + "\n";
+  exifInfo += "纬度坐标：" + latitude + "\n";
+  exifInfo += "曝光时间：" + exposureTime + " 秒\n";
+  exifInfo += "光圈大小：" + apertureValue + "\n";
+  exifInfo += "ISO：" + isoSpeedRatings + "\n";
+  exifInfo += "设备信息：" + make + " " + model;
+
+  return exifInfo;
 }
 
-// 解析经度和纬度坐标
-function parseCoordinate(coordinate, ref) {
-    const degrees = coordinate[0];
-    const minutes = coordinate[1];
-    const seconds = coordinate[2];
-    
-    const decimal = degrees + (minutes / 60) + (seconds / 3600);
-    const direction = ref === 'N' || ref === 'E' ? '' : '-';
-    
-    return `${direction}${decimal.toFixed(6)}`;
+function optimizeCoordinate(coordinate, coordinateRef) {
+  // 将度分秒坐标转换为十进制坐标
+  var degrees = coordinate[0].numerator / coordinate[0].denominator;
+  var minutes = coordinate[1].numerator / coordinate[1].denominator / 60;
+  var seconds = coordinate[2].numerator / coordinate[2].denominator / 3600;
+  var direction = coordinateRef === 'N' || coordinateRef === 'E' ? 1 : -1;
+  var decimalCoordinate = (degrees + minutes + seconds) * direction;
+  
+  return decimalCoordinate.toFixed(6);
 }
 
-// 解析分数
-function parseFraction(fraction) {
-    const numerator = fraction.numerator;
-    const denominator = fraction.denominator;
-    
-    if (denominator === 1) {
-        return numerator;
-    } else {
-        return Math.round(numerator / denominator);
-    }
+function formatDateTimeOriginal(dateTimeOriginal) {
+  // 将拍摄日期格式化为"YYYY-MM-DD HH:MM:SS"的形式
+  var date = new Date(dateTimeOriginal * 1000);
+  var formattedDate = date.getFullYear() + '-' + (date.getMonth() + 1).toString().padStart(2, '0') + '-' + date.getDate().toString().padStart(2, '0');
+  var formattedTime = date.getHours().toString().padStart(2, '0') + ':' + date.getMinutes().toString().padStart(2, '0') + ':' + date.getSeconds().toString().padStart(2, '0');
+  
+  return formattedDate + ' ' + formattedTime;
+}
+
+function formatExposureTime(exposureTime) {
+  // 将曝光时间格式化为分数
+  var numerator = exposureTime.numerator;
+  var denominator = exposureTime.denominator;
+  if (denominator >= 1 && denominator <= 100) {
+    // 分母是小数，按照四舍五入取整
+    numerator = Math.round(numerator / denominator);
+    denominator = 1;
+  }
+  
+  return numerator + '/' + denominator;
 }
