31996f31eb6b3eda6b95ed88d7cc88daa8abf0d4
wanqianshijie <135097639+wanqianshijie@users.noreply.github.com>
2023-06-30 01:20:28 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,97 +1,108 @@
-// GitHub repository information
-const username = 'wanqianshijie';
-const repository = 'photos';
+// 使用GitHub API获取照片列表
+const githubUsername = 'wanqianshijie';
+const repoName = 'photos';
 const path = 'img';
 
-// Fetch photos from GitHub repository
-fetch(`https://api.github.com/repos/${username}/${repository}/contents/${path}`)
-  .then(response => response.json())
-  .then(photos => {
-    photos.forEach(photo => {
-      const photoUrl = photo.download_url;
-
-      // Create photo element
-      const photoElement = document.createElement('img');
-      photoElement.classList.add('photo');
-      photoElement.src = photoUrl;
-
-      // Create EXIF info element
-      const exifInfoElement = document.createElement('div');
-      exifInfoElement.classList.add('exif-info');
-
-      // Get EXIF data using exif.js
-      EXIF.getData(photoElement, function() {
-        const exifData = EXIF.getAllTags(this);
-        const {
-          DateTimeOriginal,
-          GPSLatitude,
-          GPSLongitude,
-          ExposureTime,
-          FNumber,
-          ISOSpeedRatings,
-          Make,
-          Model
-        } = exifData;
-
-        // Format GPS coordinates
-        const latitude = convertCoordinate(GPSLatitude);
-        const longitude = convertCoordinate(GPSLongitude);
-
-        // Format ExposureTime as a fraction
-        const exposureTime = formatExposureTime(ExposureTime);
-
-        // Format DateTimeOriginal
-        const date = formatDate(DateTimeOriginal);
-
-        // Create EXIF info content
-        const content = `
-          <p>Date: ${date}</p>
-          <p>Latitude: ${latitude}</p>
-          <p>Longitude: ${longitude}</p>
-          <p>Exposure Time: ${exposureTime}</p>
-          <p>Aperture: ${FNumber}</p>
-          <p>ISO: ${ISOSpeedRatings}</p>
-          <p>Camera: ${Make} ${Model}</p>
-        `;
-        exifInfoElement.innerHTML = content;
-      });
-
-      // Append photo and EXIF info to the container
-      const photoContainer = document.getElementById('photo-container');
-      photoContainer.appendChild(photoElement);
-      photoContainer.appendChild(exifInfoElement);
+fetch(`https://api.github.com/repos/${githubUsername}/${repoName}/contents/${path}`)
+    .then(response => response.json())
+    .then(data => {
+        // 遍历照片列表
+        data.forEach(photo => {
+            const imageUrl = photo.download_url;
+            
+            // 创建照片元素
+            const photoElement = document.createElement('img');
+            photoElement.src = imageUrl;
+            photoElement.classList.add('photo');
+            
+            // 在照片下方创建exif信息元素
+            const exifInfoElement = document.createElement('div');
+            exifInfoElement.classList.add('exif-info');
+            
+            // 使用exif.js获取exif信息
+            EXIF.getData(photoElement, function() {
+                const exifData = EXIF.getAllTags(this);
+                
+                // 解析并显示exif信息
+                const exifInfo = parseExifData(exifData);
+                exifInfoElement.innerText = exifInfo;
+            });
+            
+            // 将照片和exif信息添加到页面上
+            const photoContainer = document.getElementById('photo-container');
+            photoContainer.appendChild(photoElement);
+            photoContainer.appendChild(exifInfoElement);
+        });
+    })
+    .catch(error => {
+        console.error('Error:', error);
     });
-  });
 
-// Helper function to convert GPS coordinates to decimal format
-function convertCoordinate(coordinate) {
-  if (!coordinate) return '';
-
-  const degrees = coordinate[0].numerator / coordinate[0].denominator;
-  const minutes = coordinate[1].numerator / coordinate[1].denominator;
-  const seconds = coordinate[2].numerator / coordinate[2].denominator;
-
-  const decimalDegrees = degrees + (minutes / 60) + (seconds / 3600);
-  const direction = coordinate[0].value === 0 ? 'S' : 'N';
-
-  return `${decimalDegrees.toFixed(6)}° ${direction}`;
+// 解析exif信息
+function parseExifData(exifData) {
+    let exifInfo = '';
+    
+    if (exifData.DateTimeOriginal) {
+        const date = new Date(exifData.DateTimeOriginal);
+        const formattedDate = formatDate(date);
+        exifInfo += `拍摄时间：${formattedDate}\n`;
+    }
+    
+    if (exifData.GPSLatitude && exifData.GPSLongitude) {
+        const latitude = parseCoordinate(exifData.GPSLatitude, exifData.GPSLatitudeRef);
+        const longitude = parseCoordinate(exifData.GPSLongitude, exifData.GPSLongitudeRef);
+        exifInfo += `经度坐标：${longitude}\n`;
+        exifInfo += `纬度坐标：${latitude}\n`;
+    }
+    
+    if (exifData.ExposureTime) {
+        const exposureTime = parseFraction(exifData.ExposureTime);
+        exifInfo += `曝光时间：${exposureTime}\n`;
+    }
+    
+    if (exifData.FNumber) {
+        exifInfo += `光圈大小：f/${exifData.FNumber}\n`;
+    }
+    
+    if (exifData.ISO) {
+        exifInfo += `ISO：${exifData.ISO}\n`;
+    }
+    
+    if (exifData.Make && exifData.Model) {
+        exifInfo += `设备信息：${exifData.Make} ${exifData.Model}\n`;
+    }
+    
+    return exifInfo;
 }
 
-// Helper function to format ExposureTime as a fraction
-function formatExposureTime(value) {
-  if (!value) return '';
-
-  const numerator = value.numerator;
-  const denominator = value.denominator;
-  return `1/${Math.round(denominator/numerator)}`;
+// 格式化日期
+function formatDate(date) {
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const day = String(date.getDate()).padStart(2, '0');
+    return `${year}-${month}-${day}`;
 }
 
-// Helper function to format date
-function formatDate(dateTime) {
-  if (!dateTime) return '';
+// 解析经度和纬度坐标
+function parseCoordinate(coordinate, ref) {
+    const degrees = coordinate[0];
+    const minutes = coordinate[1];
+    const seconds = coordinate[2];
+    
+    const decimal = degrees + (minutes / 60) + (seconds / 3600);
+    const direction = ref === 'N' || ref === 'E' ? '' : '-';
+    
+    return `${direction}${decimal.toFixed(6)}`;
+}
 
-  const [date, time] = dateTime.split(' ');
-  const [year, month, day] = date.split(':');
-
-  return `${day}/${month}/${year} ${time}`;
+// 解析分数
+function parseFraction(fraction) {
+    const numerator = fraction.numerator;
+    const denominator = fraction.denominator;
+    
+    if (denominator === 1) {
+        return numerator;
+    } else {
+        return Math.round(numerator / denominator);
+    }
 }
