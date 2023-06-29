aaaf46063b3e91da13138f0ced5b282db1002b47
wanqianshijie <135097639+wanqianshijie@users.noreply.github.com>
2023-06-30 00:54:21 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,85 +1,85 @@
-$(document).ready(function() {
-    var page = 1; // 当前页码
-    var perPage = 4; // 每页照片数量
-    var baseUrl = 'https://api.github.com/repos/wanqianshijie/photos/contents/img'; // GitHub API URL
-    var gallery = $('#gallery'); // 照片展示容器
+// GitHub repository information
+const username = 'wanqianshijie';
+const repository = 'photos';
+const path = 'img';
 
-    loadPhotos();
+// Fetch photos from GitHub repository
+fetch(`https://api.github.com/repos/${username}/${repository}/contents/${path}`)
+  .then(response => response.json())
+  .then(photos => {
+    photos.forEach(photo => {
+      const photoUrl = photo.download_url;
 
-    // 滚动到底部时加载更多照片
-    $(window).scroll(function() {
-        if ($(window).scrollTop() + $(window).height() == $(document).height()) {
-            page++;
-            loadPhotos();
-        }
+      // Create photo element
+      const photoElement = document.createElement('img');
+      photoElement.classList.add('photo');
+      photoElement.src = photoUrl;
+
+      // Create EXIF info element
+      const exifInfoElement = document.createElement('div');
+      exifInfoElement.classList.add('exif-info');
+
+      // Get EXIF data using exif.js
+      EXIF.getData(photoElement, function() {
+        const exifData = EXIF.getAllTags(this);
+        const {
+          DateTimeOriginal,
+          GPSLatitude,
+          GPSLongitude,
+          ExposureTime,
+          FNumber,
+          ISOSpeedRatings,
+          Make,
+          Model
+        } = exifData;
+
+        // Format GPS coordinates
+        const latitude = convertCoordinate(GPSLatitude);
+        const longitude = convertCoordinate(GPSLongitude);
+
+        // Format DateTimeOriginal
+        const date = formatDate(DateTimeOriginal);
+
+        // Create EXIF info content
+        const content = `
+          <p>Date: ${date}</p>
+          <p>Latitude: ${latitude}</p>
+          <p>Longitude: ${longitude}</p>
+          <p>Exposure Time: ${ExposureTime}</p>
+          <p>Aperture: ${FNumber}</p>
+          <p>ISO: ${ISOSpeedRatings}</p>
+          <p>Camera: ${Make} ${Model}</p>
+        `;
+        exifInfoElement.innerHTML = content;
+      });
+
+      // Append photo and EXIF info to the container
+      const photoContainer = document.getElementById('photo-container');
+      photoContainer.appendChild(photoElement);
+      photoContainer.appendChild(exifInfoElement);
     });
+  });
 
-    // 加载照片
-    function loadPhotos() {
-        var url = baseUrl + '?page=' + page + '&per_page=' + perPage;
-        $.getJSON(url, function(data) {
-            $.each(data, function(index, photo) {
-                var img = $('<img>').attr('src', photo.download_url).addClass('photo');
-                var info = $('<div>').addClass('info');
+// Helper function to convert GPS coordinates to decimal format
+function convertCoordinate(coordinate) {
+  if (!coordinate) return '';
 
-                // 获取照片的EXIF信息
-                getExifInfo(photo.download_url, function(exif) {
-                    var time = formatDate(exif.DateTimeOriginal);
-                    var latitude = formatCoordinate(exif.GPSLatitude, exif.GPSLatitudeRef);
-                    var longitude = formatCoordinate(exif.GPSLongitude, exif.GPSLongitudeRef);
-                    var exposureTime = exif.ExposureTime;
-                    var aperture = exif.FNumber;
-                    var iso = exif.ISOSpeedRatings;
-                    var make = exif.Make;
-                    var model = exif.Model;
+  const degrees = coordinate[0].numerator;
+  const minutes = coordinate[1].numerator;
+  const seconds = coordinate[2].numerator / coordinate[2].denominator;
 
-                    // 创建照片信息的HTML元素
-                    var infoHtml = '<p>拍摄时间：' + time + '</p>' +
-                                   '<p>经度坐标：' + longitude + '</p>' +
-                                   '<p>纬度坐标：' + latitude + '</p>' +
-                                   '<p>曝光时间：' + exposureTime + '</p>' +
-                                   '<p>光圈大小：' + aperture + '</p>' +
-                                   '<p>ISO：' + iso + '</p>' +
-                                   '<p>设备信息：' + make + ' ' + model + '</p>';
+  const decimalDegrees = degrees + (minutes / 60) + (seconds / 3600);
+  const direction = coordinate[0].value === 0 ? 'S' : 'N';
 
-                    info.html(infoHtml);
+  return `${decimalDegrees.toFixed(6)}° ${direction}`;
+}
 
-                    // 将照片和信息添加到照片展示容器中
-                    gallery.append(img);
-                    gallery.append(info);
-                });
-            });
-        });
-    }
+// Helper function to format date
+function formatDate(dateTime) {
+  if (!dateTime) return '';
 
-    // 获取照片的EXIF信息
-    function getExifInfo(url, callback) {
-        EXIF.getData(url, function() {
-            var exif = EXIF.getAllTags(this);
-            callback(exif);
-        });
-    }
+  const [date, time] = dateTime.split(' ');
+  const [year, month, day] = date.split(':');
 
-    // 格式化经度/纬度坐标
-    function formatCoordinate(coordinate, direction) {
-        var degrees = coordinate[0];
-        var minutes = coordinate[1];
-        var seconds = coordinate[2];
-
-        var formatted = degrees + '° ' + minutes + '\' ' + seconds + '\" ' + direction;
-
-        return formatted;
-    }
-
-    // 格式化日期
-    function formatDate(dateString) {
-        var date = new Date(dateString);
-        var formatted = date.getFullYear() + '-' + padZero(date.getMonth() + 1) + '-' + padZero(date.getDate());
-        return formatted;
-    }
-
-    // 补零函数
-    function padZero(num) {
-        return num < 10 ? '0' + num : num;
-    }
-});
+  return `${day}/${month}/${year} ${time}`;
+}
