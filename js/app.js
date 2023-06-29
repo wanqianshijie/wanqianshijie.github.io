e0fb7961c550a519639e3e71a0b81b8e70852215
wanqianshijie <135097639+wanqianshijie@users.noreply.github.com>
2023-06-29 18:11:03 +0800

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,46 +1,44 @@
 new Vue({
   el: '#app',
-  data: {
-    photos: [],
+  data() {
+    return {
+      photos: []
+    };
   },
-  created() {
+  mounted() {
     this.fetchPhotos();
   },
   methods: {
     fetchPhotos() {
-      const apiUrl = 'https://api.github.com/repos/wanqianshijie/photos/contents/img';
-      fetch(apiUrl)
+      const url = 'https://api.github.com/repos/wanqianshijie/photos/contents/img';
+      fetch(url)
         .then(response => response.json())
         .then(data => {
-          this.photos = data.map(photo => {
-            return {
-              id: photo.name,
-              title: photo.name,
-              url: photo.download_url,
-              exif: this.getExifInfo(photo.download_url),
+          this.photos = data.map(item => {
+            const photo = {
+              id: item.sha,
+              url: item.download_url,
+              title: item.name,
+              exif: {}
             };
+            // 使用exif.js获取照片的exif信息，并保存到photo对象中的exif属性中
+            EXIF.getData(item, function() {
+              photo.exif = {
+                datetime: EXIF.getTag(this, 'DateTimeOriginal'),
+                longitude: EXIF.getTag(this, 'GPSLongitude'),
+                latitude: EXIF.getTag(this, 'GPSLatitude'),
+                exposureTime: EXIF.getTag(this, 'ExposureTime'),
+                aperture: EXIF.getTag(this, 'FNumber'),
+                iso: EXIF.getTag(this, 'ISOSpeedRatings'),
+                camera: EXIF.getTag(this, 'Model')
+              };
+            });
+            return photo;
           });
         })
         .catch(error => {
           console.error('Error fetching photos:', error);
         });
-    },
-    getExifInfo(photoUrl) {
-      return new Promise((resolve, reject) => {
-        const image = new Image();
-        image.src = photoUrl;
-
-        image.onload = function() {
-          EXIF.getData(image, function() {
-            const exifData = EXIF.getAllTags(this);
-            resolve(exifData);
-          });
-        };
-
-        image.onerror = function() {
-          reject(new Error('Failed to load image'));
-        };
-      });
-    },
-  },
+    }
+  }
 });
