80b7f338af2f0c200f82fa6731877585a35edd98
wanqianshijie <135097639+wanqianshijie@users.noreply.github.com>
2023-06-29 18:04:22 +0800

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,9 +1,9 @@
 new Vue({
   el: '#app',
   data: {
-    photos: []
+    photos: [],
   },
-  mounted() {
+  created() {
     this.fetchPhotos();
   },
   methods: {
@@ -12,35 +12,35 @@ new Vue({
       fetch(apiUrl)
         .then(response => response.json())
         .then(data => {
-          this.photos = data.map(photo => ({
-            id: photo.name,
-            url: photo.download_url,
-            name: photo.name,
-            showExif: false,
-            exif: {}
-          }));
+          this.photos = data.map(photo => {
+            return {
+              id: photo.name,
+              title: photo.name,
+              url: photo.download_url,
+              exif: this.getExifInfo(photo.download_url),
+            };
+          });
+        })
+        .catch(error => {
+          console.error('Error fetching photos:', error);
         });
     },
-    showExif(photo) {
-      photo.showExif = !photo.showExif;
-      if (photo.showExif && Object.keys(photo.exif).length === 0) {
-        this.fetchExif(photo);
-      }
+    getExifInfo(photoUrl) {
+      return new Promise((resolve, reject) => {
+        const image = new Image();
+        image.src = photoUrl;
+
+        image.onload = function() {
+          EXIF.getData(image, function() {
+            const exifData = EXIF.getAllTags(this);
+            resolve(exifData);
+          });
+        };
+
+        image.onerror = function() {
+          reject(new Error('Failed to load image'));
+        };
+      });
     },
-    fetchExif(photo) {
-      const imageUrl = photo.url;
-      const img = new Image();
-      img.onload = () => {
-        EXIF.getData(img, function () {
-          const exifData = EXIF.getAllTags(this);
-          photo.exif = {
-            shootTime: exifData.DateTimeOriginal,
-            device: exifData.Make + ' ' + exifData.Model,
-            parameters: 'ISO: ' + exifData.ISOSpeedRatings + ', Aperture: ' + exifData.FNumber + ', Exposure: ' + exifData.ExposureTime
-          };
-        });
-      };
-      img.src = imageUrl;
-    }
-  }
+  },
 });
